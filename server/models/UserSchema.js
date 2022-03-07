7f35ed382c9019b9dea9d47b94997c7e4334fbc6
Dansando8 <d.sandoval@vertciel.ca>
2022-03-07 16:15:43 +0100

    Mongoose User schema created

---
+++ server/models/UserSchema.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true
+  }, 
+  email: {
+    type: String, 
+    required:true, 
+    unique: true
+  },
+  password: {
+    type: String, 
+    required: true
+  },
+  profilePictureURL:{ 
+    type:String, 
+    required:true
+  }
+
+}
+) 
+
+module.exports = mongoose.model('User', userSchema)
